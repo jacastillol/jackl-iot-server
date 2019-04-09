e3651ce5050e32f68b69cad6bb6bd38ddfd8113b
Jaime <jacastillol@unal.edu.co>
2019-04-09 13:49:54 -0500

    feat: example of interactions

---
+++ iot/GetMaterial.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import RPi.GPIO as GPIO
+from mfrc import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+# Reading the card
+try:
+    print('Now place your tag to write to get your material!')
+    id, text = reader.read()
+    print(id)
+    print('Card was readed', text)
+finally:
+    GPIO.cleanup()
+
+
+# sending POST request
+import requests
+
+session = requests.Session()
+data = {
+    'name':text,
+}
+req = session.post('http://userd.thejacklrobot.com/getmaterial', data=data)
+
+print(req.status_code)
