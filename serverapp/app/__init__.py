11c14b7abdf855404cff42db5002968bd02cc25d
Jaime Andres <jacastillol@unal.edu.co>
2019-05-03 19:08:23 +0000

    chore: initial app

---
+++ serverapp/app/__init__.py
@@ -0,0 +1,51 @@
+from flask import Flask
+from config import Config
+
+from flask import render_template, session, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app =  Flask(__name__)
+app.config.from_object(Config)
+bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+from werkzeug.security import generate_password_hash, check_password_hash
+    
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
